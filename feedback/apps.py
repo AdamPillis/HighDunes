feee0668e2fdd25caddef6fc34e0665d9c47677f
AdamPillis <adam.pillis@hotmail.com>
2022-02-22 13:02:59 +0000

    Create superuser and add Review and Comment models to feedback.models.py

--- feedback/apps.py
+++ feedback/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class GolfConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'golf'
+    name = 'feedback'
---
+++ feedback/models.py
@@ -0,0 +1,47 @@
+"""
+importing django's built-in model strucuture
+importing django's user model for auth purposes
+"""
+from django.db import models
+from django.contrib.auth.models import User
+
+REVIEW_STATUS = ((0, "Draft"), (1, "Published"))
+COMMENT_STATUS = ((0, "Pending"), (1, "Approved"))
+
+
+# Create your models here.
+class Review(models.Model):
+    """X"""
+    title = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=50, unique=True)
+    member = models.ForeignKey(User, on_delete=models.CASCADE, related_name="review_posts")
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=REVIEW_STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name='review_likes', blank=True)
+
+    class Meta:
+        """To set reviews order based on 'created_on' date"""
+        ordering = ['created_on']
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        """enable each review to gain unlimited likes"""
+        return self.likes.count()
+
+
+class Comment(models.Model):
+    """To enable users to leave comments under reviews"""
+    review = models.ForeignKey(Review, on_delete=models.CASCADE, related_name='comments')
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.IntegerField(choices=COMMENT_STATUS, default=0)
+
+    class Meta:
+        """To set reviews order based on 'created_on' date"""
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"{self.body}"
