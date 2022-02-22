6db36aeeff4c7a0568a7b92a1ad0635aee868429
AdamPillis <adam.pillis@hotmail.com>
2022-02-22 14:07:01 +0000

    Add: filters, display and search fields to reviews and comments in feedback.admin.py

--- feedback/admin.py
+++ feedback/admin.py
@@ -1,5 +1,27 @@
 from django.contrib import admin
-from .models import Review
+from .models import Review, Comment
+from django_summernote.admin import SummernoteModelAdmin
+
 
 # Register your models here.
-admin.site.register(Review)
+@admin.register(Review)
+class ReviewAdmin(SummernoteModelAdmin):
+    """summernote django app installed and replaces content sections for user input"""
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'likes', 'created_on')
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    summernote_fields = ('content')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """
+    To give the ability to search and filter comments while displaying key data
+    """
+    list_display = ('body', 'created_on', 'approved')
+    list_filter = ('created_on', 'approved')
+    search_fields = ['body']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
