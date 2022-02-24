061b585d504a2f367bca86b74baf15e66a03064b
AdamPillis <adam.pillis@hotmail.com>
2022-02-24 12:06:11 +0000

    Add and Link index.html with feedback.view.py and feedback.urls.py

--- feedback/views.py
+++ feedback/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
+from .models import Review
 
-# Create your views here.
+
+class ReviewList(generic.ListView):
+    """
+    Using django's built in generic view, this class
+    will list all the reviews using the imported 
+    Review model through index.html and order it
+    by the 'created_on date
+    """
+    model = Review
+    queryset = Review.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 6
