0001802b6a79da9eca550581661cf86242a1a45c
AdamPillis <adam.pillis@hotmail.com>
2022-03-02 10:49:03 +0000

    Create: review_page.html and link with each review

--- feedback/urls.py
+++ feedback/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.ReviewList.as_view(), name='home'),
+    path('<slug:slug>/', views.ReviewInDetail.as_view(), name='review_page'),
 ]
--- feedback/views.py
+++ feedback/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.views import generic
+from django.views import generic, View
 from .models import Review
 
 
@@ -14,3 +14,26 @@ class ReviewList(generic.ListView):
     queryset = Review.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 3
+
+
+class ReviewInDetail(View):
+    """X"""
+    def get(self, request, slug, *args, **kwargs):
+        """X"""
+        queryset = Review.objects.filter(status=1)
+        review = get_object_or_404(queryset, slug=slug)
+        comments = review.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        if review.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "review_page.html",
+            {
+                "review": review,
+                "comments": comments,
+                "commented": False,
+                "liked": liked
+            },
+        )
