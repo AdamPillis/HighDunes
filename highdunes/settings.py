275e5fbe05f309f4169ae5243c545ae5737e8709
AdamPillis <adam.pillis@hotmail.com>
2022-03-04 14:42:46 +0000

    Add: django messages to settings.py and link static script.js with base.html

--- highdunes/settings.py
+++ highdunes/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+from django.contrib.messages import constants as messages
 if os.path.isfile('env.py'):
     import env
 
@@ -60,6 +61,15 @@ SITE_ID = 1
 # site redirects to home page
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+# setting bootstrap 4 color classes to django's contrib messages
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
 
 # Telling cripsy forms to use bootstrap 4 template pack 
 # through which classes will work the same
