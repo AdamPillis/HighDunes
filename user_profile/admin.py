af334337392181837c7604b67510172f8b25b046
AdamPillis <adam.pillis@hotmail.com>
2022-03-09 10:32:40 +0000

    Add: filter and search functionality for profiles in admin panel

--- user_profile/admin.py
+++ user_profile/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import Profile
 
-admin.site.register(Profile)
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    """
+    To give the admin the ability to filter and search for 
+    specific profiles
+    """
+    list_display = ('created_on', 'first_name', 'last_name')
+    list_filter = ('first_name', 'last_name', 'email', 'phone_number', 'country', 'created_on')
+    search_fields = ['last_name']
