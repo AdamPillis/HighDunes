908737681332d4d6f76f289a4ae8ea5c7764b018
AdamPillis <adam.pillis@hotmail.com>
2022-03-17 19:31:55 +0000

    Add: DateInput widget to play_date field in golf_booking.forms.py

--- golf_booking/forms.py
+++ golf_booking/forms.py
@@ -6,11 +6,22 @@ from django import forms
 from .models import Booking
 
 
+class DateInput(forms.DateInput):
+    """
+    Using DateInput from django forms
+    to allow user to enter date into
+    Booking form correctly and with ease
+    """
+    input_type = 'date'
+
+
 class BookingForm(forms.ModelForm):
     """
     Booking form created using django's built in
     forms to display a form for the user to fill
-    in to book tee time
+    in to book tee time.
+    Using DateInput set above as widget to set 
+    play_date = DateField()
     """
     class Meta:
         """
@@ -18,6 +29,7 @@ class BookingForm(forms.ModelForm):
         to include in form
         """
         model = Booking
+        widgets = {'play_date': DateInput()}
         fields = [
             'number_of_holes',
             'number_of_players',
